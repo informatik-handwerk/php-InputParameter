871b080f035829d49b4122eb3a812b3528aadb42
Peter Fargas <github.com@informatik-handwerk.de>
2022-04-06 22:07:44 +0200

    #5 InputParameter_Single_*

--- src/Impl/InputParameter_Single_String.php
+++ src/Impl/InputParameter_Single_String.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace ihde\php\InputParameter\Impl;
 
 use ihde\php\InputParameter\InputParameter_Single;
+use ihde\php\InputParameter\Lang\Instantiable_KeyValue;
 
 class InputParameter_Single_String
     extends InputParameter_Single {
@@ -13,14 +14,34 @@ class InputParameter_Single_String
     
     /**
      * @param string $name
-     * @param string $input
+     * @param string $value
      */
-    public function __construct(string $name, string $input) {
-        parent::__construct($name);
-        $this->value = $input;
+    protected function __construct(string $name, string $value) {
+        parent::__construct($name, $value);
+        $this->value = $value;
     }
     
     /**
+     * @param        $name
+     * @param string $string
+     * @return InputParameter_Single_String
+     */
+    public static function instance_direct($name, string $string): self {
+        $instance = new static($name, $string);
+        return $instance;
+    }
+    
+    /**
+     * @implements Instantiable_KeyValue
+     * @inheritDoc
+     */
+    public static function instance_keyValue($key, $value): self {
+        $instance = new static($key, $value);
+        return $instance;
+    }
+    
+    /**
+     * @inheritDoc
      * @return string
      */
     public function getValue(): string {
